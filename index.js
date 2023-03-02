0a8d255c105d5322f483fa8a0ecf72cebfb1a7c2
jbrobarge <jbrobarge@gmail.com>
2023-03-01 20:40:55 -0800

    moles appear

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 let score = 0;
 // document.getElementsByClassName("hole");
 let holes = document.getElementsByClassName("hole");
-let randomHole = Math.floor(Math.random() * holes.length);
-holes[randomHole].classList.toggle("mole");
+setInterval(function () {
+  let randomHole = Math.floor(Math.random() * holes.length);
+  holes[randomHole].classList.toggle("mole");
+}, 500);
