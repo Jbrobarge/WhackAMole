eb32c661268e10a634b0a83a666ea4ac3175d693
jbrobarge <jbrobarge@gmail.com>
2023-03-01 20:32:13 -0800

    mole in hole

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 let score = 0;
 // document.getElementsByClassName("hole");
 let holes = document.getElementsByClassName("hole");
+let randomHole = Math.floor(Math.random() * holes.length);
+holes[randomHole].classList.toggle("mole");
