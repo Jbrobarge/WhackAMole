95cca1a851a42a8b8477a0d3fdf654ec1fa78215
jbrobarge <jbrobarge@gmail.com>
2023-03-01 20:52:49 -0800

    score works moles disappear

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 let score = 0;
+let scoreDisplay = document.getElementById("score");
 // document.getElementsByClassName("hole");
 let holes = document.getElementsByClassName("hole");
 setInterval(function () {
   let randomHole = Math.floor(Math.random() * holes.length);
   holes[randomHole].classList.toggle("mole");
-}, 500);
+}, 1000);
+
+const gameArea = document.getElementById("whack-a-mole");
+gameArea.addEventListener("click", function (clickEvent) {
+  if (clickEvent.target.matches(".mole")) {
+    clickEvent.target.classList.remove("mole");
+    score++;
+    scoreDisplay.innerText = score;
+  }
+});
